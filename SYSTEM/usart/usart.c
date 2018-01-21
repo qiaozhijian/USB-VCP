c40ff57ecd7769404e338944c7e6996c3ba064c6
Summer <935514540@qq.com>
2018-01-21 13:39:51 +0800

    4156

---
+++ SYSTEM/usart/usart.c
@@ -0,0 +1,291 @@
+#include "sys.h"
+#include "usart.h"	
+////////////////////////////////////////////////////////////////////////////////// 	 
+//如果使用ucos,则包括下面的头文件即可.
+#if SYSTEM_SUPPORT_UCOS
+#include "includes.h"					//ucos 使用	  
+#endif
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK STM32F4探索者开发板
+//串口1初始化		   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2014/6/10
+//版本：V1.5
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
+//All rights reserved
+//********************************************************************************
+//V1.3修改说明 
+//支持适应不同频率下的串口波特率设置.
+//加入了对printf的支持
+//增加了串口接收命令功能.
+//修正了printf第一个字符丢失的bug
+//V1.4修改说明
+//1,修改串口初始化IO的bug
+//2,修改了USART_RX_STA,使得串口最大接收字节数为2的14次方
+//3,增加了USART_REC_LEN,用于定义串口最大允许接收的字节数(不大于2的14次方)
+//4,修改了EN_USART3_RX的使能方式
+//V1.5修改说明
+//1,增加了对UCOSII的支持
+////////////////////////////////////////////////////////////////////////////////// 	  
+ 
+
+//////////////////////////////////////////////////////////////////
+//加入以下代码,支持printf函数,而不需要选择use MicroLIB	  
+#if 1
+#pragma import(__use_no_semihosting)             
+//标准库需要的支持函数                 
+struct __FILE 
+{ 
+	int handle; 
+}; 
+
+FILE __stdout;       
+//定义_sys_exit()以避免使用半主机模式    
+void _sys_exit(int x)
+{ 
+	x = x; 
+} 
+//重定义fputc函数 
+int fputc(int ch, FILE *f)
+{ 	
+	while((USART3->SR&0X40)==0);//循环发送,直到发送完毕   
+	USART3->DR = (u8) ch;      
+	return ch;
+}
+#endif
+ 
+#if EN_USART3_RX   //如果使能了接收
+//串口1中断服务程序
+//注意,读取USARTx->SR能避免莫名其妙的错误   	
+u8 USART_RX_BUF[USART_REC_LEN];     //接收缓冲,最大USART_REC_LEN个字节.
+//接收状态
+//bit15，	接收完成标志
+//bit14，	接收到0x0d
+//bit13~0，	接收到的有效字节数目
+u16 USART_RX_STA=0;       //接收状态标记	
+
+//初始化IO 串口1 
+//bound:波特率
+void uart_init(u32 bound){
+   //GPIO端口设置
+  GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB,ENABLE); //使能GPIOB时钟
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3,ENABLE);//使能USART3时钟
+ 
+	//串口1对应引脚复用映射
+	GPIO_PinAFConfig(GPIOB,GPIO_PinSource11,GPIO_AF_USART3); //GPIOB9复用为USART3
+	GPIO_PinAFConfig(GPIOB,GPIO_PinSource10,GPIO_AF_USART3); //GPIOB10复用为USART3
+	
+	//USART3端口配置
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11 | GPIO_Pin_10; //GPIOB9与GPIOB10
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;//复用功能
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;	//速度50MHz
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP; //推挽复用输出
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP; //上拉
+	GPIO_Init(GPIOB,&GPIO_InitStructure); //初始化PA9，PA10
+
+   //USART3 初始化设置
+	USART_InitStructure.USART_BaudRate = bound;//波特率设置
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;//字长为8位数据格式
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;//一个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;//无奇偶校验位
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//无硬件数据流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;	//收发模式
+  USART_Init(USART3, &USART_InitStructure); //初始化串口1
+	
+  USART_Cmd(USART3, ENABLE);  //使能串口1 
+	
+	USART_ClearFlag(USART3, USART_FLAG_TC);
+	
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);//开启相关中断
+
+	//USART3 NVIC 配置
+  NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;//串口1中断通道
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3;//抢占优先级3
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority =3;		//子优先级3
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQ通道使能
+	NVIC_Init(&NVIC_InitStructure);	//根据指定的参数初始化VIC寄存器、
+	
+}
+
+
+void USART3_IRQHandler(void)                	//串口1中断服务程序
+{
+	u8 Res;
+#ifdef OS_TICKS_PER_SEC	 	//如果时钟节拍数定义了,说明要使用ucosII了.
+	OSIntEnter();    
+#endif
+	if(USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)  //接收中断(接收到的数据必须是0x0d 0x0a结尾)
+	{
+		Res =USART_ReceiveData(USART3);//(USART3->DR);	//读取接收到的数据
+		
+		if((USART_RX_STA&0x8000)==0)//接收未完成
+		{
+			if(USART_RX_STA&0x4000)//接收到了0x0d
+			{
+				if(Res!=0x0a)USART_RX_STA=0;//接收错误,重新开始
+				else USART_RX_STA|=0x8000;	//接收完成了 
+			}
+			else //还没收到0X0D
+			{	
+				if(Res==0x0d)USART_RX_STA|=0x4000;
+				else
+				{
+					USART_RX_BUF[USART_RX_STA&0X3FFF]=Res ;
+					USART_RX_STA++;
+					if(USART_RX_STA>(USART_REC_LEN-1))USART_RX_STA=0;//接收数据错误,重新开始接收	  
+				}		 
+			}
+		}   		 
+  } 
+#ifdef OS_TICKS_PER_SEC	 	//如果时钟节拍数定义了,说明要使用ucosII了.
+	OSIntExit();  											 
+#endif
+} 
+#endif	
+
+ 
+ /****************************************************************************
+* 名    称：void USART_OUT(USART_TypeDef* USARTx, uint8_t *Data,...)
+* 功    能：格式化串口输出函数
+* 入口参数：USARTx:  指定串口
+			Data：   发送数组
+			...:     不定参数
+* 出口参数：无
+* 说    明：格式化串口输出函数
+        	"\r"	回车符	   USART_OUT(USART3, "abcdefg\r")   
+			"\n"	换行符	   USART_OUT(USART3, "abcdefg\r\n")
+			"%s"	字符串	   USART_OUT(USART3, "字符串是：%s","abcdefg")
+			"%d"	十进制	   USART_OUT(USART3, "a=%d",10)
+* 调用方法：无 
+****************************************************************************/
+void USART_OUT(USART_TypeDef* USARTx, const char *Data, ...)
+{ 
+	const char *s;
+    int d;
+    char buf[16];
+    va_list ap;
+    va_start(ap, Data);
+
+	while(*Data != 0)				                          //判断是否到达字符串结束符
+	{
+		if(*Data == 0x5c)									  //'\'
+		{	
+			switch (*++Data)
+			{
+				case 'r':							          //回车符
+					USART_SendData(USARTx, 0x0d);	   
+
+					Data++;
+					break;
+				case 'n':							          //换行符
+					USART_SendData(USARTx, 0x0a);	
+					Data++;
+					break;
+				
+				default:
+					Data++;
+				    break;
+			}	 
+		}
+		else if(*Data == '%')									  //
+		{
+			switch (*++Data)				
+			{
+				case 's':										  //字符串
+                	s = va_arg(ap, const char *);
+                	for (; *s; s++) 
+				    {
+                    	USART_SendData(USARTx, *s);
+						while(USART_GetFlagStatus(USARTx, USART_FLAG_TC) == RESET);
+                	}
+					Data++;
+                	break;
+            	case 'd':										  //十进制
+                	d = va_arg(ap, int);
+                	itoa(d, buf, 10);
+                	for (s = buf; *s; s++) 
+				    {
+                    	USART_SendData(USARTx, *s);
+						while(USART_GetFlagStatus(USARTx, USART_FLAG_TC) == RESET);
+                	}
+					Data++;
+                	break;
+				default:
+					Data++;
+				    break;
+			}		 
+		}
+		else USART_SendData(USARTx, *Data++);
+		while(USART_GetFlagStatus(USARTx, USART_FLAG_TC) == RESET);
+	}
+}
+/******************************************************
+		整形数据转字符串函数
+        char *itoa(int value, char *string, int radix)
+		radix=10 标示是10进制	非十进制，转换结果为0;  
+
+	    例：d=-379;
+		执行	itoa(d, buf, 10); 后
+		
+		buf="-379"							   			  
+**********************************************************/
+char *itoa(int value, char *string, int radix)
+{
+    uint32_t i;
+	int d;
+    int     flag = 0;
+    char    *ptr = string;
+
+    /* This implementation only works for decimal numbers. */
+    if (radix != 10)
+    {
+        *ptr = 0;
+        return string;
+    }
+
+    if (!value)
+    {
+        *ptr++ = 0x30;
+        *ptr = 0;
+        return string;
+    }
+
+    /* if this is a negative value insert the minus sign. */
+    if (value < 0)
+    {
+        *ptr++ = '-';
+
+        /* Make the value positive. */
+        value *= -1;
+    }
+
+    for (i = 100000000; i > 0; i /= 10)
+    {
+        d = value / i;
+
+        if (d || flag)
+        {
+            *ptr++ = (char)(d + 0x30);
+            value -= (d * i);
+            flag = 1;
+        }
+    }
+
+    /* Null terminate the string. */
+    *ptr = 0;
+
+    return string;
+
+} 
+ 
+
+
+
+
---
+++ USER/main.c
@@ -0,0 +1,186 @@
+/**
+  ******************************************************************************
+  * @file    app.c
+  * @author  MCD Application Team
+  * @version V1.1.0
+  * @date    19-March-2012
+  * @brief   This file provides all the Application firmware functions.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */ 
+
+/* Includes ------------------------------------------------------------------*/ 
+#include "usart.h"
+#include "delay.h"
+#include "led.h"
+#include "usbd_cdc_core.h"
+#include "usbd_usr.h"
+#include "usb_conf.h"
+#include "usbd_desc.h"
+#include "usbd_cdc_vcp.h"
+#include "usb_dcd_int.h"
+
+
+/** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
+  * @{
+  */
+
+
+/** @defgroup APP_VCP 
+  * @brief Mass storage application module
+  * @{
+  */ 
+
+/** @defgroup APP_VCP_Private_TypesDefinitions
+  * @{
+  */ 
+/**
+  * @}
+  */ 
+
+
+/** @defgroup APP_VCP_Private_Defines
+  * @{
+  */ 
+
+/**
+  * @}
+  */ 
+
+
+/** @defgroup APP_VCP_Private_Macros
+  * @{
+  */ 
+/**
+  * @}
+  */ 
+
+
+/** @defgroup APP_VCP_Private_Variables
+  * @{
+  */ 
+  
+#ifdef USB_OTG_HS_INTERNAL_DMA_ENABLED
+  #if defined ( __ICCARM__ ) /*!< IAR Compiler */
+    #pragma data_alignment=4   
+  #endif
+#endif /* USB_OTG_HS_INTERNAL_DMA_ENABLED */
+   
+__ALIGN_BEGIN USB_OTG_CORE_HANDLE    USB_OTG_dev __ALIGN_END ;
+
+/**
+  * @}
+  */ 
+
+
+/** @defgroup APP_VCP_Private_FunctionPrototypes
+  * @{
+  */ 
+/**
+  * @}
+  */ 
+
+
+/** @defgroup APP_VCP_Private_Functions
+  * @{
+  */ 
+
+/**
+  * @brief  Program entry point
+  * @param  None
+  * @retval None
+  */
+	
+	
+int main(void)
+{
+	u8 dat[12] = {"Hello world"};
+  __IO uint32_t i = 0;  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
+	delay_init(168);  //初始化延时函数
+	uart_init(115200);		//初始化串口波特率为115200
+	LED_Init();					//初始化LED  
+  /*!< At this stage the microcontroller clock setting is already configured, 
+  this is done through SystemInit() function which is called from startup
+  file (startup_stm32fxxx_xx.s) before to branch to application main.
+  To reconfigure the default setting of SystemInit() function, refer to
+  system_stm32fxxx.c file
+  */  
+ 
+  USBD_Init(&USB_OTG_dev,
+#ifdef USE_USB_OTG_HS 
+            USB_OTG_HS_CORE_ID,
+#else            
+            USB_OTG_FS_CORE_ID,
+#endif  
+            &USR_desc, 
+            &USBD_CDC_cb, 
+            &USR_cb);
+  
+  /* Main loop */
+  while (1)
+  {
+		LED0=!LED0;
+  USART_OUT(USART3,"Hello world!\r\n");
+		//if(USB_RecvieCount){
+		//	printf("RecDatalen:%d Start TxData\r\n",USB_RecvieCount);
+			APP_FOPS.pIf_DataTx(dat, 12);
+			USB_RecvieCount = 0;
+		//}
+		delay_ms(500);
+		//printf("USB_RecvieCount:%x\r\n",USB_RecvieCount);
+  }
+} 
+
+#ifdef USE_FULL_ASSERT
+/**
+* @brief  assert_failed
+*         Reports the name of the source file and the source line number
+*         where the assert_param error has occurred.
+* @param  File: pointer to the source file name
+* @param  Line: assert_param error line source number
+* @retval None
+*/
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+  ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  
+  /* Infinite loop */
+  while (1)
+  {}
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+
+/**
+  * @}
+  */ 
+
+
+/**
+  * @}
+  */ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
